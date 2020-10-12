d28434da6279839c45a581b5411852329180a7cc
michals-lab <59738119+michals-lab@users.noreply.github.com>
2020-10-12 15:20:35 +0200

    zmiana numeracji

---
+++ CW_00/src/pl/edu/po/cw_01/Start.java
@@ -0,0 +1,32 @@
+package pl.edu.po.cw_01;
+
+public class Start {
+    public static void main(String[] args){
+        Zad6();
+        Zad8();
+        Zad11();
+    }
+
+    static void Zad6() {
+        System.out.println("  /////");
+        System.out.println(" +\"\"\"\"\"+");
+        System.out.println("(| o o |)");
+        System.out.println("  | ^ |");
+        System.out.println(" | `-' |");
+        System.out.println(" +-----+");
+    }
+
+    static void Zad8() {
+        System.out.println("   +");
+        System.out.println("  + +");
+        System.out.println(" +   +");
+        System.out.println("+-----+");
+        System.out.println("| .-. |");
+        System.out.println("| | | |");
+        System.out.println("+-+-+-+");
+    }
+
+    static void Zad11() {
+        System.out.println("Ale Beliar nie znosił widoku światła \ni niszczył wszystko, co Innos stworzył. \n\nUjrzał wtedy Adanos, \nże w ten sposób nic nie może istnieć na świecie \n– ani jasność, ani mrok. \n\nStanął więc między swymi braćmi \ni próbował pojednać ich między sobą… \njednakże bez skutku. \n\nAle tam, gdzie stanął Adanos, \nani Innos, ani Beliar nie mieli żadnej władzy. \nW miejscu tym ład i chaos współistniały w harmonii \n\n– Tak o to powstało morze. \n\nA z morza wyłonił się ląd, \na na lądzie powstało wszystko to, co żywe… \nRośliny i zwierzęta, wilki i owce, \n\na na samym końcu powstali ludzie. \n\nAdanos cieszył się, ze wszystkiego, co wówczas powstało, \na swą miłością darzył jednakowo wszystkie rzeczy. \n\nAle gniew Beliara był tak wielki, \nże przemierzył on całą Ziemię, \nby znaleźć bestię. \nA gdy do niej przemówił, \nstała się ona jego sługą. \n\nBeliar tchnął w nią część swej boskiej mocy, \nby mogła zniszczyć całą Ziemię. \n\nAle Innos podpatrzył czyn Beliara i przemierzył Ziemię, \nby znaleźć człowieka. \nA gdy do niego przemówił, \nten stał się jego sługą. \n\nInnos tchnął w niego część swojej boskiej mocy, \nby mógł on naprawić szkody wyrządzone przez Beliara. \n\nWtedy zwrócił się Beliar do innej istoty, \nale Adanos zesłał potężną falę, \nktóra zmyła ją z powierzchni Ziemi. \n\nAle wraz z nią odeszły także drzewa i zwierzęta, \nwięc Adanos wielce się zasmucił. \nI rzekł wreszcie do swych braci \n\n– „Nigdy więcej nie postanie wasza noga na mojej Ziemi, bo jest ona święta i pozostanie taka na wieki”. \n\nAle człowiek i bestia nie zaprzestali wojny na Ziemi Adanosa \ni płonął w nich gniew ich bogów. \n\nA człowiek pokonał bestię, \nktóra wróciła do królestwa Beliara. \nI zobaczył Adanos, \nże prysła równowaga między ładem a chaosem i zaklął Innosa, \nby ten odebrał człowiekowi swą boską moc. \n\nA Innos w swej mądrości tak uczynił. \n\nAle Adanos obawiał się, \nże pewnego dnia bestia powróci na Ziemię. \nDlatego uprosił Innosa, \nby ten zostawił część swojej mocy na Ziemi, \naby pewnego dnia przywrócić ją człowiekowi. \n\nA Innos w swej mądrości tak uczynił.");
+    }
+}
